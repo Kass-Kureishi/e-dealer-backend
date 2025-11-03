5aebf4e2921ca0dda3206d269cbff84fbf02e350
ahmerkureishi <ahmerkureishi@gmail.com>
2025-11-03 07:00:55 -0500

    Fix: Add missing User model and dependencies

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
   email: {
     type: String,
     required: true,
@@ -16,43 +21,24 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['tenant', 'landlord', 'agent', 'admin'],
-    default: 'tenant'
-  },
-  profile: {
-    firstName: String,
-    lastName: String,
-    phone: String,
-    avatar: String,
-    bio: String
-  },
-  verification: {
-    isVerified: {
-      type: Boolean,
-      default: false
-    },
-    token: String
+    enum: ['user', 'admin'],
+    default: 'user'
   }
 }, {
-  timestamps: true // This adds createdAt and updatedAt automatically
+  timestamps: true
 });
 
 // Hash password before saving
-UserSchema.pre('save', async function(next) {
+userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
-  try {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
-// Method to compare password
-UserSchema.methods.comparePassword = async function(candidatePassword) {
+// Compare password method
+userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
